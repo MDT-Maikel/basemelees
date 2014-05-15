e69bf2efaf87f3369aef4a6a13855b7345819a1a
Maikel de Vries <mdt.maikel@gmail.com>
2014-05-15 10:50:02 +0200

    medal for critical hit

---
+++ Objects.ocd/Medals.ocd/Medals.ocd/CriticalHit.ocd/Script.c
@@ -0,0 +1,68 @@
+/**
+	Critical Hit
+	Awarded for dealing more than 40 damage in a single hit.
+
+	@author Maikel
+*/
+
+
+/*-- Medal Properties --*/
+
+local Name = "$Name$";
+local Description = "$Description$";
+
+public func IsMedal() { return true; }
+public func GetMedalIndex() { return 6; }
+
+
+/*-- Medal Scripts --*/
+
+// See Medal_CriticalHit.c script for handling energy loss of the crew member.
+
+// Called when the round starts, technically when the rule is created.
+public func OnRoundStart()
+{
+	AddEffect("IntMedalCriticalHit", nil, 100, 0, nil, Medal_CriticalHit);
+	return;
+}
+
+// Called when the round is finished, i.e. when all players are elimanted
+// or all goals are fulfilled.
+public func OnRoundFinish()
+{
+	RemoveEffect("IntMedalCriticalHit", nil);
+	return;
+}
+
+
+/*-- Medal Effects --*/
+
+// Medal critical hit effect keeps track of whether the medal may be awarded.
+public func FxIntMedalCriticalHitStart(object target, proplist effect, int temporary)
+{
+	if (temporary)
+		return FX_OK;
+	return FX_OK;
+}
+
+// Effect callback made when a crews energy has been changed.
+public func FxIntMedalCriticalHitOnCrewDamage(object target, proplist effect, int damage, int to_plr, int by_plr)
+{
+	// Only a critical hit if damage >= 40.
+	if (damage < 40)
+		return FX_OK;
+	// Only valid if done to an enemy.
+	if (by_plr == NO_OWNER || !Hostile(to_plr, by_plr))
+		return FX_OK;
+	// Award the medal.
+	Rule_Medals->~AwardMedal(Medal_CriticalHit, by_plr);
+	return FX_OK;
+}
+
+// Medal critical hit effect keeps track of whether the medal may be awarded.
+public func FxIntMedalCriticalHitStop(object target, proplist effect, int reason, bool temporary)
+{
+	if (temporary)
+		return FX_OK;
+	return FX_OK;
+}
---
+++ Objects.ocd/Medals.ocd/Scripts.ocd/System.ocg/Medal_CriticalHit.c
@@ -0,0 +1,15 @@
+// Appendto script for the critical hit. Tracks eneergy change in crew memebers.
+
+#appendto Clonk
+
+// Callback from the engine when the energy of a living object changes.
+protected func OnEnergyChange(int change, int cause, int by_plr)
+{
+	//Log("%s energy: change %d, cause %d, by player %d", GetName(), change, cause, by_plr);
+	// Forward energy change to the critical hit effect.
+	var damage = -change / 1000;
+	var effect = GetEffect("IntMedalCriticalHit", nil);
+	if (effect)
+		EffectCall(nil, effect, "OnCrewDamage", damage, this->GetOwner(), by_plr);
+	return _inherited(change, cause, by_plr, ...);
+}
