fc7a141aa2be67ecb8e547358218d92f56143f1a
Maikel de Vries <mdt.maikel@gmail.com>
2016-04-08 20:29:38 +0200

    update base material functions

--- Functions.ocd/System.ocg/BaseMaterial.c
+++ Functions.ocd/System.ocg/BaseMaterial.c
@@ -30,21 +30,21 @@ global func GivePlayerBasicBaseMaterial(int plr, bool add_mats)
 {
 	var base_mats = [
 		// Clonks & health.
-		[Clonk,     10,  2],
-		[Bread,      5,  1],
+		[Clonk,          10,  2],
+		[Bread,           5,  1],
 		// Materials & mining.
-		[Wood,      10,  4],
-		[Metal,     10,  4],
-		[Rock,      10,  4],
-		[Cloth,      4,  2],
-		[Loam,       4,  2],
-		[Firestone,  4,  2],
-		[Dynamite,   4,  2],
+		[Wood,           10,  4],
+		[Metal,          10,  4],
+		[Rock,           10,  4],
+		[Cloth,           4,  2],
+		[Loam,            4,  2],
+		[Firestone,       4,  2],
+		[Dynamite,        4,  2],
 		// Essential tools.
-		[Shovel,     4,  2],
-		[Hammer,     2,  1],
-		[Axe,        2,  1],
-		[Pickaxe,    2,  1]
+		[Shovel,          4,  2],
+		[Hammer,          2,  1],
+		[Axe,             2,  1],
+		[Pickaxe,         2,  1]
 	];
 	for (var mat in base_mats)
 	{
@@ -67,8 +67,8 @@ global func GivePlayerNormalBaseMaterial(int plr, bool add_mats)
 {
 	var base_mats = [
 		// Additional: Clonks & health.
-		[Clonk,      4,  1],
-		[Bread,      2,  1],
+		[Clonk,           4,  1],
+		[Bread,           2,  1],
 		// Additional: Materials & mining.
 		[Wood,            2,  1],
 		[Metal,           2,  1],
@@ -76,23 +76,24 @@ global func GivePlayerNormalBaseMaterial(int plr, bool add_mats)
 		[Cloth,           2,  1],
 		[Firestone,       2,  2],
 		[Dynamite,        2,  2],
+		[DynamiteBox,     2,  1],
 		// Additional tools.
-		[Barrel,       2,  1],
-		[MetalBarrel,  2,  1],
-		[Bucket,  2,  1],
-		[Torch,  2,  1],
-		[Pipe,  2,  1],
-		[WallKit,  2,  1],
+		[Barrel,          2,  1],
+		[MetalBarrel,     2,  1],
+		[Bucket,          2,  1],
+		[Torch,           2,  1],
+		[Pipe,            2,  1],
+		[WallKit,         2,  1],
 		// Weapons & explosives.
-		[Bow, 1,  1],
-		[Arrow, 2,  1],
-		[Shield, 2,  1],
-		[Javelin, 2,  1],
-		[Club, 1,  1],
+		[Bow,             2,  1],
+		[Arrow,           2,  1],
+		[Shield,          2,  1],
+		[Javelin,         2,  1],
+		[Club,            1,  1],
 		// Advanced tools.
-		[Ropeladder, 2, 1],
-		[GrappleBow, 2, 1],
-		[Balloon, 2, 1]
+		[Ropeladder,      2,  1],
+		[GrappleBow,      2,  1],
+		[Balloon,         2,  1]
 	];
 	for (var mat in base_mats)
 	{
@@ -124,19 +125,24 @@ global func GivePlayerAdvancedBaseMaterial(int plr, bool add_mats)
 		[Cloth,           2,  1],
 		[Firestone,       2,  2],
 		[Dynamite,        2,  2],
+		// Additional: weapons.
+		[Bow,             1,  1],
+		[Arrow,           2,  1],
+		[Club,            1,  1],
 		// Advanced tools.
 		[TeleGlove,       1,  1],
 		[WindBag,         1,  1],
 		[Boompack,        1,  1],
 		// Advanced weapons.
-		[FireArrow,       1,  1],
+		[FireArrow,       2,  1],
 		[BombArrow,       2,  1],
 		[Sword,           2,  1],
-		[Club,            1,  1],
 		[Musket,          1,  1],
 		[LeadShot,        1,  1],
 		[GrenadeLauncher, 1,  1],
 		[IronBomb,        2,  1],
+		[SmokeBomb,       2,  1],
+		[Lantern,         2,  1],
 		[Catapult,        1,  1],
 		[PowderKeg,       2,  1],
 		[Cannon,          1,  1]
