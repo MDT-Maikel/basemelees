fb5603f2363171ea6ef73c5bac38d4a5fcd017a3
Maikel de Vries <mdt.maikel@gmail.com>
2016-04-09 15:41:15 +0200

    make slasher medal more failsafe

--- Objects.ocd/Medals.ocd/Medals.ocd/Slasher.ocd/Append.ocd/System.ocg/SwordSlash.c
+++ Objects.ocd/Medals.ocd/Medals.ocd/Slasher.ocd/Append.ocd/System.ocg/SwordSlash.c
@@ -1,13 +1,13 @@
 // Appendto script for the slasher medal. Tracks sword strikes by crew members.
 
-#appendto Sword
+#appendto Library_MeleeWeapon
 
 public func ApplyWeaponBash(object to, int strength, int angle, object from)
 {
 	var real_from = from ?? this;
 	var striker = real_from->Contained();
-	// Let slasher medal effect know a sword slash has happened.
-	if (striker && GetEffect("VisualJumpStrike", striker))
+	// If this is a sword let slasher medal effect know a sword slash has happened.
+	if (GetID() == Sword && striker && GetEffect("VisualJumpStrike", striker))
 	{
 		var effect = GetEffect("IntMedalSlasher", nil);
 		if (effect)
