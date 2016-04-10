de90e59bb676727014b7b3d68a59321bf0f148eb
Maikel de Vries <mdt.maikel@gmail.com>
2016-04-10 10:51:30 +0200

    fix miner medal

--- Objects.ocd/Medals.ocd/Medals.ocd/Miner.ocd/Append.ocd/System.ocg/OnSale.c
+++ Objects.ocd/Medals.ocd/Medals.ocd/Miner.ocd/Append.ocd/System.ocg/OnSale.c
@@ -6,20 +6,16 @@
 #appendto Amethyst
 
 
-// Callback from the seller library, is done just before selling an item.
+// Callback from the vendor library, is done when an item has been sold.
 // This also allows to keep track of who brought in the valuable resource.
-public func QueryOnSell(int controller)
+public func OnSale(int controller, object at_base)
 {
-	// First evaluate _inherited(controller, ...) to find if an actual sale is done.
-	var no_sell = _inherited(controller, ...);
-	if (no_sell)
-		return true;
 	// Get value of the resource.
-	var value = GetValue();
+	var value = at_base->GetSellValue(this);
 	// Notify the effect that keeps track of the mining that a valuable has been sold.
 	var effect = GetEffect("IntMedalMiner", nil);
 	if (effect)
 		EffectCall(nil, effect, "HasMined", controller, this->GetID(), value);
-	// Allow the selling.
-	return false;
+	// Also perform the callback in other scripts.
+	return _inherited(controller, at_base, ...);
 }
