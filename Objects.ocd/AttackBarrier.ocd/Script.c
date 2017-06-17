180fbc822f2668957d6bd31182d35c174d3ca3f7
Maikel de Vries <mdt.maikel@gmail.com>
2017-06-17 15:35:31 +0200

    improve attack barriers

--- Objects.ocd/AttackBarrier.ocd/Script.c
+++ Objects.ocd/AttackBarrier.ocd/Script.c
@@ -1,19 +1,31 @@
 /**
 	Attack Barriers
 	Barriers preventing early attacks and allowing for building up a settlement.
-	Create blocks with the following ID calls:
+	Create blocks with the following definition calls:
 	* BlockLine(int x1, int y1, int x2, int y2, int time)
 	* BlockRectangle(int x, int y, int wdt, int hgt, int time)
-
+	BlockRectangle must have wdt >= 56 and hgt >= 56.
 	@author Maikel
 */
 
 
-
 protected func Initialize()
 {
-	SetGraphics(Format("%d", 1 + Random(4)));
 	SetSolidMask(0, 0, 40, 8);
+	return;
+}
+
+public func MakeEdge()
+{
+	SetGraphics("Edge");
+	SetSolidMask(0, 8, 40, 8);
+	return;
+}
+
+public func MakeEnd()
+{
+	SetGraphics("End");
+	SetSolidMask(0, 16, 40, 8);
 	return;
 }
 
@@ -33,6 +45,16 @@ public func BlockLine(int x1, int y1, int x2, int y2, int time)
 		block->SetPosition(x, y);
 		block->SetR(block_angle);
 		ScheduleCall(block, "RemoveObject", time, 0);
+		// Adapt barriers at the end.
+		if (block_nr == 0)
+		{
+			block->MakeEnd();
+			block->SetR(block->GetR() + 180);
+		}
+		if (block_nr == nr_of_blocks)
+		{
+			block->MakeEnd();
+		}
 	}
 	return;
 }
@@ -40,24 +62,44 @@ public func BlockLine(int x1, int y1, int x2, int y2, int time)
 // Blocks a rectangle for the specified number of frames.
 public func BlockRectangle(int x, int y, int wdt, int hgt, int time)
 {
+	// Check minimum width and height.
+	wdt = Max(wdt, 56);
+	hgt = Max(hgt, 56);	
 	// Create top and bottom barriers.
-	for (var lx = x + 20; lx < x + wdt + 20; lx +=40)
+	for (var lx = x + 24; lx < x + wdt + 16; lx += 40)
 	{
-		var top = CreateObject(AttackBarrier, Min(lx, x + wdt - 20) , y);
-		var bottom = CreateObject(AttackBarrier, Min(lx, x + wdt - 20), y + hgt);
+		var top = CreateObject(AttackBarrier, Min(lx, x + wdt - 24) , y);
+		var bottom = CreateObject(AttackBarrier, Min(lx, x + wdt - 24), y + hgt);
 		ScheduleCall(top, "RemoveObject", time, 0);
 		ScheduleCall(bottom, "RemoveObject", time, 0);
 	}
 	// Create left and right barriers.
-	for (var ly = y + 20; ly < y + hgt + 20; ly +=40)
+	for (var ly = y + 24; ly < y + hgt + 16; ly += 40)
 	{
-		var left = CreateObject(AttackBarrier, x, Min(ly, y + hgt - 20));
-		var right = CreateObject(AttackBarrier, x + wdt, Min(ly, y + hgt - 20));
+		var left = CreateObject(AttackBarrier, x, Min(ly, y + hgt - 24));
+		var right = CreateObject(AttackBarrier, x + wdt, Min(ly, y + hgt - 24));
 		left->SetR(90);
 		right->SetR(90);
 		ScheduleCall(left, "RemoveObject", time, 0);
 		ScheduleCall(right, "RemoveObject", time, 0);
 	}
+	// Create rectangle edges.
+	var edge;
+	edge = CreateObject(AttackBarrier, x, y - 16);
+	edge->SetR(270);
+	edge->MakeEdge();
+	ScheduleCall(edge, "RemoveObject", time, 0);
+	edge = CreateObject(AttackBarrier, x + wdt + 16, y);
+	edge->MakeEdge();
+	ScheduleCall(edge, "RemoveObject", time, 0);
+	edge = CreateObject(AttackBarrier, x - 16, y + hgt);
+	edge->SetR(180);
+	edge->MakeEdge();
+	ScheduleCall(edge, "RemoveObject", time, 0);
+	edge = CreateObject(AttackBarrier, x + wdt, y + hgt + 16);
+	edge->SetR(90);
+	edge->MakeEdge();
+	ScheduleCall(edge, "RemoveObject", time, 0);
 	return;
 }
 
