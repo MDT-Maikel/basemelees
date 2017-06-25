ea59d0919cd70a598f46bf2837110725d560f838
Maikel de Vries <mdt.maikel@gmail.com>
2017-06-25 20:07:02 +0200

    update cannon test scenario

--- CannonTest.ocs/Script.c
+++ CannonTest.ocs/Script.c
@@ -8,13 +8,139 @@
 
 static script_plr;
 
-protected func Initialize()
+public func Initialize()
 {
+	// No power need for pumping and tesla cannon.
+	CreateObject(Rule_NoPowerNeed);
 
+	// Create different cannons and test grounds.
+	InitLiquidCannon();
+	InitGatlingGun();
+	InitCrossbow();
+	InitArtilleryCannon();
+	InitTeslaCannon();
 	return;
 }
 
-protected func InitializePlayer(int plr)
+public func InitLiquidCannon()
+{
+	// Create liquid cannons and connect to all types of liquid pumps.
+	var pump1 = CreateObjectAbove(Pump, 756, 448);
+	var pump2 = CreateObjectAbove(Pump, 724, 448);
+	var pump3 = CreateObjectAbove(Pump, 692, 448);
+	var pump4 = CreateObjectAbove(Pump, 660, 448);
+	var pump5 = CreateObjectAbove(Pump, 652, 352);
+	pump1->SetMaterialSelection([Water]);
+	pump2->SetMaterialSelection([Lava]);
+	pump3->SetMaterialSelection([Acid]);
+	pump4->SetMaterialSelection([Water]);
+	pump5->SetMaterialSelection([Concrete]);
+	
+	var foundry = CreateObjectAbove(Foundry, 700, 352);
+	foundry->CreateContents(Rock, 100);
+	foundry->AddToQueue(Concrete, nil, true);
+	
+	CreateObject(Pipe, 756, 484)->ConnectPipeTo(pump1, PIPE_STATE_Source);
+	CreateObject(Pipe, 724, 484)->ConnectPipeTo(pump2, PIPE_STATE_Source);
+	CreateObject(Pipe, 692, 484)->ConnectPipeTo(pump3, PIPE_STATE_Source);
+	CreateObject(Pipe, 660, 484)->ConnectPipeTo(pump4, PIPE_STATE_Source);
+	var source = CreateObjectAbove(Pipe, 660, 484);
+	source->ConnectPipeTo(pump5, PIPE_STATE_Source);
+	source->ConnectPipeTo(foundry, PIPE_STATE_Source);
+	
+	var tower1 = CreateObjectAbove(GuardTower, 800, 448);
+	var tower2 = CreateObjectAbove(GuardTower, 744, 352);	
+	var frame1 = CreateObjectAbove(CannonFrame, 812, 446);
+	var frame2 = CreateObjectAbove(CannonFrame, 610, 350);
+	var cannon1 = CreateObject(LiquidCannon, 800, 400);
+	var cannon2 = CreateObject(LiquidCannon, 800, 400);
+	var cannon3 = CreateObject(LiquidCannon, 800, 400);
+	var cannon4 = CreateObject(LiquidCannon, 800, 400);
+	cannon1->MountCannon(tower1->GetCannonMount());
+	cannon2->MountCannon(frame1);
+	cannon3->MountCannon(tower2->GetCannonMount());
+	cannon4->MountCannon(frame2);
+	
+	var drain1 = CreateObjectAbove(Pipe, pump4->GetX(), pump4->GetY());
+	drain1->ConnectPipeTo(pump4, PIPE_STATE_Drain);
+	drain1->ConnectPipeTo(foundry, PIPE_STATE_Drain);
+	var drain2 = CreateObjectAbove(Pipe, pump1->GetX(), pump1->GetY());
+	drain2->ConnectPipeTo(pump1, PIPE_STATE_Drain);
+	drain2->ConnectPipeTo(cannon2, PIPE_STATE_Drain);
+	var drain3 = CreateObjectAbove(Pipe, pump2->GetX(), pump2->GetY());
+	drain3->ConnectPipeTo(pump2, PIPE_STATE_Drain);
+	drain3->ConnectPipeTo(cannon1, PIPE_STATE_Drain);
+	var drain4 = CreateObjectAbove(Pipe, pump3->GetX(), pump3->GetY());
+	drain4->ConnectPipeTo(pump3, PIPE_STATE_Drain);
+	drain4->ConnectPipeTo(cannon3, PIPE_STATE_Drain);
+	var drain5 = CreateObjectAbove(Pipe, pump5->GetX(), pump5->GetY());
+	drain5->ConnectPipeTo(pump5, PIPE_STATE_Drain);
+	drain5->ConnectPipeTo(cannon4, PIPE_STATE_Drain);
+		
+	// Create a forest which can be burned and extinguished.
+	PlaceForest([Tree_Deciduous, Tree_Coniferous2], 840, LandscapeHeight(), 204, true);
+	for (var tree in FindObjects(Find_Func("IsTree"), Find_InRect(840, 0, 204, LandscapeWidth())))
+	{
+		tree.NoBurnDecay = true;
+		tree->CreateEffect(FxBlockDamage, 100);
+		tree->SetClrModulation(RGB(128, 128, 100));
+	}
+	
+	// Place some bats which can be killed with the acid spray.
+	Bat->Place(20, nil, Rectangle(800, 0, 224, 400));
+	return;
+}
+
+public func InitGatlingGun()
+{
+	// Create loaded gatling gun.
+	var frame = CreateObjectAbove(CannonFrame, 160, 446);
+	var gatling_gun = CreateObjectAbove(GatlingGun, 160, 446);
+	gatling_gun->CreateContents(LeadBullet, 10);
+	gatling_gun->MountCannon(frame);
+	
+	// Create strawman field to shoot at.
+	for (var index = 0; index < 120; index++)
+		CreateObjectAbove(StrawMan, RandomX(6, 108), 448);
+	return;
+}
+
+public func InitCrossbow()
+{
+	// Create loaded crossbow.
+	var frame = CreateObjectAbove(CannonFrame, 240, 446);
+	var crossbow = CreateObjectAbove(Crossbow, 240, 446);
+	crossbow->CreateContents(Arrow, 8);
+	crossbow->CreateContents(FireArrow, 8);
+	crossbow->CreateContents(BombArrow, 8);
+	crossbow->MountCannon(frame);
+	
+	// Create flying rockets as targets.
+	CreateEffect(FxCreateRockets, 100, 54);
+	return;
+}
+
+public func InitArtilleryCannon()
+{
+	var frame = CreateObjectAbove(CannonFrame, 320, 446);
+	var artillery_cannon = CreateObjectAbove(ArtilleryCannon, 320, 446);
+	artillery_cannon->CreateContents(PowderKeg);
+	artillery_cannon->MountCannon(frame);
+	var chest = CreateObjectAbove(Chest, 340, 448);
+	chest->CreateContents(IronBomb, 20);
+	chest->CreateContents(PowderKeg, 10);
+	return;
+}
+
+public func InitTeslaCannon()
+{
+	var frame = CreateObjectAbove(CannonFrame, 384, 222);
+	var tesla_cannon = CreateObjectAbove(TeslaCannon, 384, 222);
+	tesla_cannon->MountCannon(frame);
+	return;
+}
+
+public func InitializePlayer(int plr)
 {
 	// Set zoom to full map size.
 	SetPlayerZoomByViewRange(plr, LandscapeWidth(), nil, PLRZOOM_Direct);
@@ -22,79 +148,25 @@ protected func InitializePlayer(int plr)
 	// No FoW to see everything happening.
 	SetFoW(false, plr);
 	
+	// Move clonk to middle.
 	var clonk = GetCrew(plr);
-	clonk->SetPosition(90, 150);
-	
-	CreateObject(Rule_NoPowerNeed);
-	
-	var frame = CreateObjectAbove(CannonFrame, 90, 140);
-	var artillery_cannon = CreateObjectAbove(ArtilleryCannon, 70, 140);
-	artillery_cannon->CreateContents(PowderKeg);
-	CreateObjectAbove(Chest, 30, 160)->CreateContents(IronBomb, 10);
-	var gatling_gun = CreateObjectAbove(GatlingGun, 120, 80);
-	gatling_gun->CreateContents(LeadBullet, 10);
-	var liquid_cannon = CreateObjectAbove(LiquidCannon, 50, 140);
-	//liquid_cannon->MountCannon(frame);
-	var crossbow = CreateObjectAbove(Crossbow, 30, 140);
-	crossbow->CreateContents(Arrow);
-	crossbow->CreateContents(FireArrow);	
-	crossbow->CreateContents(BombArrow);
-	crossbow->MountCannon(frame);
-	
-	var tesla_cannon = CreateObjectAbove(TeslaCannon, 110, 150);
-		
-	var pump = CreateObjectAbove(Pump, 54, 160, plr);
-	var source = CreateObjectAbove(Pipe, 168, 292, plr);
-	source->ConnectPipeTo(pump, PIPE_STATE_Source);
-	var drain = CreateObjectAbove(Pipe, 240, 100, plr);
-	drain->ConnectPipeTo(pump, PIPE_STATE_Drain);
-	drain->ConnectPipeTo(liquid_cannon, PIPE_STATE_Drain);
-	
-	var tower = CreateObjectAbove(GuardTower, 120, 160);
-	var tower_base = CreateObjectAbove(Basement, 120, 168);
-	tower_base->SetParent(tower);
-	
-	//CreateObjectAbove(Flagpole, 20, 160, plr);
-	//CreateObjectAbove(WindGenerator, 20, 160, plr);
-	
-	tesla_cannon->MountCannon(tower->GetCannonMount());
+	clonk->SetPosition(580, 440);
 	return;
 }
 
-global func RestoreLiquidLevels(string mat)
+static const FxCreateRockets = new Effect
 {
-	if (!mat)
-		mat = "Water";
-
-	// Restore water levels.
-	DrawMaterialQuad(mat, 144, 168, 208 + 1, 168, 208 + 1, 304, 144, 304, true);
-	for (var x = 216; x <= 280; x++)
-		for (var y = 24; y <= 120; y++)
-			if (GetMaterial(x, y) != Material("BrickSoft"))
-				ClearFreeRect(x, y, 1, 1);
-	return;
-}
-
-global func CreateFire(bool to_be_inc)
-{
-	for (var index = 0; index < 200; index++)
+	Timer = func(int time)
 	{
-		if (to_be_inc)
-			CreateObjectAbove(Wood, RandomX(220, 400), RandomX(40, 160))->SetCategory(C4D_StaticBack);
-		else
-		{
-			var burn = CreateObjectAbove(Metal, RandomX(220, 400), RandomX(40, 160));
-			burn->SetCategory(C4D_StaticBack);
-			burn.NoBurnDecay = true;
-			burn->Incinerate();
-		}
+		var boomattack = CreateObject(DefenseBoomAttack, 1024, RandomX(10, 30));
+		boomattack->SetR(270);
+		boomattack.FlySpeed = RandomX(200, 250);
+		boomattack->SetWaypoints([
+			{X = 724, Y = RandomX(10, 30)},
+			{X = RandomX(520, 560), Y = RandomX(100, 140)},
+			{X = RandomX(420, 460), Y = RandomX(320, 360)},
+			{X = RandomX(160, 200), Y = RandomX(320, 360)},
+			{X = 0, Y = RandomX(180, 220)}
+		]);	
 	}
-	return;
-}
-
-global func CreateTargets()
-{
-	for (var index = 0; index < 50; index++)
-		CreateObjectAbove(StrawMan, RandomX(220, 400), RandomX(40, 160))->SetCategory(C4D_StaticBack);
-	return;
-}
+};
